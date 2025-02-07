6aeaaf2a64b2ae6ab356ea1cf7440fac1dab7a37
De-J <devjtitan@gmail.com>
2025-02-08 00:09:00 +0530

    basic setup for EC2

--- lib/blackcandy-stack.ts
+++ lib/blackcandy-stack.ts
@@ -1,19 +1,55 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import 'dotenv/config';
 
 export class BlackcandyStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'BlackcandyQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const vpc = ec2.Vpc.fromLookup(this, 'defaultVpc', {
+      vpcId: process.env.VPC_ID
     });
 
-    const topic = new sns.Topic(this, 'BlackcandyTopic');
+    const securityGroup = new ec2.SecurityGroup(
+      this,
+      'blackcandySG',
+      {
+        vpc,
+        allowAllOutbound: true,
+      }
+    );
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH from anywhere',
+    )
+
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP traffic from anywhere',
+    )
+
+    const keyPair = ec2.KeyPair.fromKeyPairName(
+      this,
+      'KeyPair',
+      process.env.KEY_PAIR ?? ''
+    );
+
+    const ec2Instance = new ec2.Instance(this, 'blackcandy', {
+      vpc,
+      securityGroup,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
+      }),
+      keyPair,
+    })
+
   }
 }
